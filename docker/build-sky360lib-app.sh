98b7e95b2144d9621cbbc577b4977f7659f87f7f
fcbarros <fabio.c.barros@gmail.com>
2023-07-25 12:58:25 +0200

    fix: change the Dockerfile name

--- docker/build-sky360lib-app.sh
+++ docker/build-sky360lib-app.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 version=1.0.4
-docker buildx build --push --platform linux/amd64,linux/arm64 -f Dockerfile-simple2 . -t sky360/sky360lib-app:$version -t sky360/sky360lib-app:latest --target sky360lib-app
+docker buildx build --push --platform linux/amd64,linux/arm64 -f Dockerfile . -t sky360/sky360lib-app:$version -t sky360/sky360lib-app:latest --target sky360lib-app
