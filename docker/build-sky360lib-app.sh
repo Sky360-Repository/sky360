7332634bb7d081d8524af5df23fb29aea7f9680a
fcbarros <fabio.c.barros@gmail.com>
2023-08-10 17:35:51 +0200

    bugfix: Dockerfile generation

--- docker/build-sky360lib-app.sh
+++ docker/build-sky360lib-app.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-version=1.0.4
+version=1.0.5
 docker buildx build --push --platform linux/amd64,linux/arm64 -f Dockerfile . -t sky360/sky360lib-app:$version -t sky360/sky360lib-app:latest --target sky360lib-app
