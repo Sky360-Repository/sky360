fa47e4cdc965d3b3deeaeea3ae6c3107b697dff4
SnIcK <ido.samuelson@gmail.com>
2023-09-14 04:35:12 -0500

    fixing local runner, disabled sudo for sky360, update snick user

--- nixOS/Systems/Base/Users/snick.nix
+++ nixOS/Systems/Base/Users/snick.nix
@@ -8,11 +8,21 @@
     home = "/home/snick";
     extraGroups = ["networkmanager" "wheel" "video" "audio" "plugdev" "dialout"];
     shell = pkgs.fish;
-    hashedPassword = "$6$9juK..70Y/0HZSda$ZeKqjVjGTOEajN5DG.d7NBSEFQRDtlIQcuhxk9xTBFqBzHYLlq90NfJNUGKhpc2mU1zfvNFl2FdmSuPclYSGL1";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ7ayPIBqQlTt2uTa0lrkUXC4Ou+rk+MG6fLn2OfATSb snick@sky360.org"
     ];
 
+    security.sudo.extraRules = [
+      {
+        users = ["snick"];
+        commands = [
+          {
+            command = "ALL";
+            options = ["NOPASSWD"]; # "SETENV" # Adding the following could be a good idea
+          }
+        ];
+      }
+    ];
     #packages = with pkgs; [
     #];
   };
